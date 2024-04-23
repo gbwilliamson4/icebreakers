276aef21c5b7dad2a350b238eab8f6dda9639224
gbwilliamson4 <90010953+gbwilliamson4@users.noreply.github.com>
2024-04-23 08:43:16 -0600

    adjust route in /api/question

--- src/app/api/question/route.ts
+++ src/app/api/question/route.ts
@@ -2,7 +2,9 @@ import axios, { AxiosResponse } from "axios";
 import { NextResponse } from "next/server";
 
 export async function GET() {
-  const route: string = process.env.RANDOM_QUESTION_ROUTE!;
+  const route: string | undefined = process.env.RANDOM_QUESTION_ROUTE;
+
+  if (!route) return new NextResponse("Error", { status: 500 });
 
   const response: AxiosResponse = await axios.get(route);
   const responseData: string = response.data.question;
