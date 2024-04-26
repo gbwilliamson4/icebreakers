4c668254821a3c63a4bea13df50be18cab7cb05a
gbwilliamson4 <90010953+gbwilliamson4@users.noreply.github.com>
2024-04-26 09:13:42 -0600

    Cleanup API route

--- src/app/api/question/route.ts
+++ src/app/api/question/route.ts
@@ -1,19 +1,16 @@
 import axios, { AxiosResponse } from "axios";
-import { NextResponse } from "next/server";
 import { cookies } from "next/headers";
+import { NextResponse } from "next/server";
 
 export async function GET() {
   const cookieStore = cookies();
-  // const route: string | undefined = process.env.RANDOM_QUESTION_ROUTE;
   const route: string | undefined = process.env.RANDOM_QUESTION_ROUTE;
 
-  if (!route) return new NextResponse("Error", { status: 500 });
+  if (!route)
+    return new NextResponse("Error. API route not defined", { status: 500 });
 
   const response: AxiosResponse = await axios.get(route);
   const responseData: string = response.data.question;
-
-  // console.log("responseData");
-  // console.log(responseData);
 
   return new NextResponse(responseData, { status: 200 });
 }
